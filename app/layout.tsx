188ad2d8d361a62731cfbb4fda9a6f0373f4ab12
everlastingvoyage <everlastingvoyage8@gmail.com>
2026-08-05 15:15:40 -0700

    Load V12 desktop session console styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ import './v11-5-session-ux.css';
 import './v11-6-completion-layout.css';
 import './v11-7-completion-clarity.css';
 import './v11-8-completion-polish.css';
+import './v12-0-desktop-session-console.css';
 import HumanoidSwap from './HumanoidSwap';
 import SignalChamberPolish from './SignalChamberPolish';
 import SignalChamberBlend from './SignalChamberBlend';
