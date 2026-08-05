8f7354eb35088725d93facccd65262a30d399c8b
everlastingvoyage <everlastingvoyage8@gmail.com>
2026-08-04 23:04:21 -0700

    Load V10.8 Blue Chamber refinement

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import './v10-5-builder-cta.css';
 import './v10-6-completion-flow.css';
 import './v10-7-frequency-chamber.css';
 import './v10-7-frequency-chamber-safari.css';
+import './v10-8-blue-chamber-refinement.css';
 import HumanoidSwap from './HumanoidSwap';
 import SignalChamberPolish from './SignalChamberPolish';
 import SignalChamberBlend from './SignalChamberBlend';
